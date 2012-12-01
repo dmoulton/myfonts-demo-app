290a16d3dedb3facfbebe461b015f40e3458921e
David Moulton <dave@themoultons.net>
2012-12-01 12:02:57 -0700

    first login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testfonts::Application.config.secret_token = '2e6cae7cb8aab71bce201da857770c4f4b89cc46efc55c3cce10158c004f47080e51eefa4b2347181292a703b6c9485531093095486d4828479f32ac9c9beb86'
